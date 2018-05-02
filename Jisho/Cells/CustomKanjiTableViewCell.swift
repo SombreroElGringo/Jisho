31549422d52cf25c4f7fd1e6c79dd0687b5ef047
SombreroElGringo <florent.pailhes@hotmail.fr>
2018-05-02 20:12:02 +0200

    - Kanji views init in the storyboard - Kanji files added

---
+++ Jisho/Cells/CustomKanjiTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomKanjiTableViewCell.swift
+//  Jisho
+//
+//  Created by Florent on 02/05/2018.
+//  Copyright © 2018 Florent. All rights reserved.
+//
+
+import UIKit
+
+class CustomKanjiTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
